122f7c9b1fcf8e9e0d1e16d8ba7372b538cb8541
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-12-08 22:49:13 -0800

    updated

--- receipt.cpp
+++ receipt.cpp
@@ -1,20 +1,71 @@
 //Author: Bianca Cervantes
 #include "receipt.h"
 #include<iostream>
+#include<vector>
+void receipt::processing(double total,std::string type)
+{       categ=type;
+        cost=total;
+        if(type=="food")
+        { nom.push_back(total); }
 
+        if(type=="bills")
+        { util.push_back(total); }
 
-void receipt::setPurchase(double total)
-{
-cost=total;
+        if(type=="education")
+        { school.push_back(total); }
+
+        if(type=="misc")
+        { rand.push_back(total); }
+
+}
+//vectors-> school,rand,util,nom
+
+double receipt::foodTotals()
+{ 
+        double total=0;
+        for(i=0;i<nom.size();i++)
+        {
+                total+=nom[i];
+        }
+        return total/nom.size();
 }
 
-void receipt::addCategory(std::string sort)
+double receipt::eduTotals()
 {
-categ=sort;
+        double total=0;
+        for(i=0;i<school.size();i++)
+        {
+                total+=school[i];
+        }
+        return total/school.size();
 }
+
+
+
+double receipt::billTotals()
+{       double total=0;
+        for(i=0;i<util.size();i++)
+        {
+                total+=util[i];
+        }
+        return total/util.size();
+}
+
+
+
+double receipt::miscTotals()
+{       double total=0;
+        for(i=0;i<rand.size();i++)
+        {
+                total+=rand[i];
+        }
+        return total/rand.size();
+}
+
+
 
 receipt::receipt()
 {
-cost=0.0;
-categ="misc"; //purchase category ie. education,food, etc.
+        cost=0.0;
+        categ="misc"; //purchase category ie. education,food, etc.
 }
--- receipt.h
+++ receipt.h
@@ -1,17 +1,23 @@
 //Author: Bianca Cervantes
-#include<string>
 #ifndef _RECEIPT_H
 #define _RECEIPT_H
+#include<string>
 
 class receipt
 {
         private:
                 double cost;
                 std::string categ;
-
+                vector<double>school;//education
+                vector<double>rand;//random ( misc )
+                vector<double>util;//utilities
+                vector<double>nom; //food
         public:
                 receipt();
-                void setPurchase(double);
-                void addCategory(std::string);
+                void processing(double, std::string);
+                double foodTotals(); 
+                double eduTotals();
+                double billTotals();
+                double miscTotals();
 };
 #endif
