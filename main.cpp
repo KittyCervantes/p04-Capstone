45c26a37a9eca87af078b0a01c91814d24ca47e4
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-12-08 22:49:55 -0800

    further added components

--- main.cpp
+++ main.cpp
@@ -6,29 +6,33 @@
 
 using namespace std;
 
-void imputReceipt(string);
-string invalidAnswer(string);
-vector<double> printTotals(double);
+string imputReceipt(int &, int &);
+void invalidAnswer(string &);
+void printTotals(int, int, int,double,double,double,double);
 
 int main()
-{
-        string method;
+{       receipt invoice;
+        string type;
         char input;
-        
+        int yours=0;
+        int borrowed=0;
+        double cost,sum,r,u,n,e;
+
 
         //welcome user
         cout<<"-------Welcome to your expenses organizer!-------"<<endl<<"Please enter your receipt information as prompted"<<endl;
 
         do
         {
-                cout<<"Was the purchase made with debit or credit?"<<endl;
-                cin>>method;
-                if(method!="debit" && method!="credit")
-                {
-                        invalidAnswer(method);
-                }
 
-                imputReceipt(method); //different categories for debit or credit
+                type=imputReceipt(yours, borrowed);
+
+                cout<<"Expense total: ";
+                cin>>cost;
+                sum+=cost;
+
+
+                invoice.processing(cost,type);
 
                 cout<<"Thank You! Would you like to add another receipt?"<<endl<<"If yes, press any key, otherwise press (x)"<<endl;
                 cin>>input;
@@ -36,46 +40,71 @@ int main()
                 //do while loop to keep inputting receipts
         }while( input!= 'x' && input!='X');
 
+
+        r= invoice.miscTotals();
+        u= invoice.billTotals();
+        n= invoice.foodTotals();
+        e= invoice.eduTotals();
+
+        printTotals(yours,borrowed,sum,r,u,n,e);
+
+
         return 0;
 }
 
 
-void imputReceipt(string currency)
+string imputReceipt(int & yours, int & borrowed)
 { 
         double cost;
-        string type;
-        receipt invoice;
+        string type,currency;
+        int grand;
 
-        cout<<"Expense total: ";
-        cin>>cost;
- 
-        invoice.setPurchase(cost);
+        cout<<"Was the purchase made with debit or credit?"<<endl;
+        cin>>currency;
+
+        if(currency!="debit" && currency!="credit")
+        {
+                invalidAnswer(currency);
+        }
+
+        if(currency=="debit")
+        {
+                yours++;
+        }
+        if(currency=="credit")
+        {
+                borrowed++;
+        }
 
         cout<<"Is the receipt for 'food', 'bills', 'education', or 'misc' ?"<<endl;
         cin>>type;
-        if(type!="food" && type!="bills" && type!="education" && type!="misc")
+
+        while(type!="food" && type!="bills" && type!="education" && type!="misc")
         {
                 invalidAnswer(type);
         }
-        else
-        {
-                invoice.addCategory(type);
-        }
+
+        return type;
 
 }
 
-string invalidAnswer(string retry)
+void invalidAnswer(string & retry)
 {
         cout<<"The text entered was not a valid answer, please only use the options provided."<<endl;
         cin>>retry;
-        return retry;
 }
 
-vector<double> printTotals(double avg)
-{
-        vector<double> activity;
-        activity.resize(1);
+void printTotals(int yours, int borrowed, int todo, double r,double u,double n,double e) //todo means all
+{receipt invoice;
 
-        return activity;
-        //temporary filler, edit this later
+        cout<<"------------------------Here's your average Spending totals!-------------------------"<<endl;
+        cout<<"School: "<<e<<" , Food: "<<n<<" , Bills: "<<u<<" , Misc: "<<r<<endl;
+        cout<<"Your total average spending is: "<<todo/(yours+borrowed)<<endl;
+
+
+        if(borrowed>yours)
+        {
+                cout<<"You seem to use your credit card very often. Depending on it too much can be bad.\n Try cutting down on your credit usage to avoid getting in over your head!"<<endl;
+        }
+
 }
